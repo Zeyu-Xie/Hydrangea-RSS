42fee840cc0afa7255bbefdf8526cbbc8ae1e313
Acan <110075452+Zeyu-Xie@users.noreply.github.com>
2024-08-16 23:38:41 +0800

    .

--- Hydrangea RSS/ContentView.swift
+++ Hydrangea RSS/ContentView.swift
@@ -1,20 +1,18 @@
-//
-//  ContentView.swift
-//  Hydrangea RSS
-//
-//  Created by Zeyu Xie on 2024/8/16.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            Text("Contents")
+                .tabItem {
+                    Image(systemName: "square.fill.text.grid.1x2")
+                    Text("Contents")
+                }
+            Settings()
+                .tabItem {
+                    Image(systemName: "gear")
+                    Text("Settings")
+                }
         }
-        .padding()
     }
 }
